145e0118bbe0d906859c5b70e6b7e382f97c802d
Andres Guzman <andguzman15@hotmail.com>
2014-09-29 21:26:43 -0500

    implementacion de la aplicacacion helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,25 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Pagina de inicio (Home)" do
+  describe "Pagina de Inicio (Home)" do
     it "debe tener el contenido 'Samle App' " do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "debe tener el titulo 'Sample App / Pagina de inicio" do
+    it "debe tener el titulo 'Predefinido' " do
     	visit '/static_pages/home'
-    	expect(page).to have_title('Sample App / Pagina de inicio')
-	end 
-  end
-
-
+    	expect(page).not_to have_title('Sample App / Home')
+	 end 
+end
 
 describe "Pagina de ayuda" do
 	it "Debe tener el contenido 'Ayuda' " do
 		visit '/static_pages/help'
       expect(page).to have_content('Ayuda')
 	end
-	it "debe tener el titulo 'Sample App / Ayuda" do
+	it "debe tener el titulo 'Sample App / Ayuda' " do
     	visit '/static_pages/help'
-    	expect(page).to have_title('Sample App / Ayuda')
+    	expect(page).not_to have_title('Sample App / Ayuda')
 	end
 end
 
@@ -30,9 +28,9 @@ describe "Pagina Acerca de" do
 		visit '/static_pages/acerca_de'
       expect(page).to have_content('Acerca de')
 	end
-	it "debe tener el titulo 'Sample App / Acerca de" do
+	it "debe tener el titulo 'Sample App / Acerca de' " do
     	visit '/static_pages/acerca_de'
-    	expect(page).to have_title('Sample App / Acerca de')
+    	expect(page).not_to have_title('Sample App / Acerca de')
 	end
 end 
 
